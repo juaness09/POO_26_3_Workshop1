45d2fb0fc21b39ea56127d4962a2f0bd0522a9e3
juaness09 <juanstan08@gmail.com>
2026-09-20 22:29:04 -0500

    ContarPalabras2

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -310,10 +310,10 @@ public boolean esPalindromo(String cadena) {
     }
     
 
-public int contarPalabras(String cadena) {
-        if (cadena == null || cadena.trim().isEmpty()) {
-            return 0;
-        }
+      public int contarPalabras(String cadena) {
+          if (cadena == null || cadena.trim().isEmpty()) {
+             return 0;
+         }
 
         String[] palabras = cadena.trim().split("\\s+");
         return palabras.length;
